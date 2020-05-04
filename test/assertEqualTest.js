01fa9dad6f9a246a561a8d695350347465073736
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-04 18:25:48 +0000

    Created new tests for head, tail, eqArrays

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,37 +1,7 @@
 const assertEqual = require('../assertEqual')
-const head = require('../head')
-const tail = require('../tail')
-const eqArrays = require('../eqArrays')
 
 
-// Testing Assert Equal
+// Testing assertEqual
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
-// Testing Head
-
-assertEqual(head([]), 5);
-assertEqual(head(["Hello"]), "Hello");
-
-// Testing Tail
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
-
-// Testing eqArrays
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);  
-  
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);  
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);  
-  
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
