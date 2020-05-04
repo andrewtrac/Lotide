90949a6059fad17c2e4c336a4b5ea11942294bc8
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-04 18:50:54 +0000

    incorporated Mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,27 @@
-const assertEqual = require('../assertEqual')
+/*const assertEqual = require('../assertEqual')
 const head = require('../head')
 
-assertEqual(head([]), 5);
 assertEqual(head(["Hello"]), "Hello");
+*/
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns undefined for an empty array", () => {
+      assert.strictEqual(head([]), undefined)
+  })
+
+  it("returns 'Hello' from the string, 'Hello'", () => {
+      assert.strictEqual(head(['Hello', '1']), 'Hello')
+  })
+
+});
