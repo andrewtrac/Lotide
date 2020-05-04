c572a09231c462c723eec7fbcac5449e2f9e7732
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-04 20:14:49 +0000

    Added object shorthand in the index

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+const head   = require('./head')
+const tail   = require('./tail')
+const middle = require('./middle')
 const assertEqual = require('./assertEqual')
 const assertArraysEqual = require('./assertArraysEqual')
 const eqArrays = require('./eqArrays')
 
 
 module.exports = {
-    head:   head,
-    tail:   tail,
-    middle: middle,
-    assertEqual: assertEqual,
-    assertArraysEqual: assertArraysEqual,
-    eqArrays: eqArrays,
+    head,   
+    head,   
+    tail,   
+    middle,
+    assertEqual, 
+    assertArraysEqual,
+    eqArrays,
+}
+    
 
-};
+
+
